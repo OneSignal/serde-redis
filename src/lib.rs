7815b8cca047ee50fc9c402bc5e7015329ffbc66
Joe Wilm <joe@jwilm.com>
2016-01-28 13:25:06 -0800

    Add `from_redis_value` helper

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,27 @@ pub mod decode;
 
 pub use encode::Serializer;
 pub use decode::Deserializer;
+
+/// Use serde Deserialize to build `T` from a `redis::Value`
+pub fn from_redis_value<T>(rv: ::redis::Value) -> decode::Result<T>
+    where T: serde::de::Deserialize
+{
+    let mut de = Deserializer::new(rv).unwrap();
+    ::serde::de::Deserialize::deserialize(&mut de)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use redis::Value;
+
+    #[test]
+    fn from_redis_value_works() {
+        let v = Value::Bulk(vec![Value::Int(5), Value::Data(b"hello".to_vec())]);
+
+        let actual: (u8, String) = from_redis_value(v).unwrap();
+        let expected = (5, "hello".into());
+
+        assert_eq!(expected, actual);
+    }
+}
