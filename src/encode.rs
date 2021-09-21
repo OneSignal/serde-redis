7e79edd1c7f7ca85d4f08bc3a7654a16018fbee6
Mostafa <mostafa.sedaghat@gmail.com>
2021-09-21 21:49:06 +0800

    Adding serialization for simple struct

--- src/encode.rs
+++ src/encode.rs
@@ -1,1 +1,341 @@
-pub struct Serializer;
+use redis::Value;
+use serde::{self, ser, Serialize};
+use std::borrow::Cow;
+use std::collections::BTreeMap;
+use std::fmt::{self, Display};
+use std::iter::Peekable;
+use std::{error, num, str, string, vec};
+
+pub struct Serializer {
+    output: Vec<Value>,
+}
+
+pub fn to_vec<T>(value: &T) -> Result<Vec<Value>>
+where
+    T: Serialize,
+{
+    let mut serializer = Serializer { output: Vec::new() };
+    value.serialize(&mut serializer)?;
+    Ok(serializer.output)
+}
+
+/// Error that can be produced during serialization
+#[derive(Debug)]
+pub enum Error {
+    Custom(String),
+}
+
+impl Error {}
+
+pub type Result<T> = ::std::result::Result<T, Error>;
+
+impl error::Error for Error {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            _ => None,
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::Custom(ref reason) => write!(f, "CustomError({})", reason),
+        }
+    }
+}
+
+impl ser::Error for Error {
+    /// Raised when there is general error when deserializing a type.
+    fn custom<T: Display>(msg: T) -> Self {
+        Error::Custom(msg.to_string())
+    }
+}
+
+impl<'a> serde::Serializer for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    type SerializeSeq = Self;
+    type SerializeTuple = Self;
+    type SerializeTupleStruct = Self;
+    type SerializeTupleVariant = Self;
+    type SerializeMap = Self;
+    type SerializeStruct = Self;
+    type SerializeStructVariant = Self;
+
+    fn serialize_bool(self, v: bool) -> Result<()> {
+        todo!()
+    }
+    fn serialize_i8(self, v: i8) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_i16(self, v: i16) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_i32(self, v: i32) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_i64(self, v: i64) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_u8(self, v: u8) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_u16(self, v: u16) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_u32(self, v: u32) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_u64(self, v: u64) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_f32(self, v: f32) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_f64(self, v: f64) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_char(self, v: char) -> Result<()> {
+        self.serialize_str(&v.to_string())
+    }
+
+    fn serialize_str(self, v: &str) -> Result<()> {
+        self.output.push(Value::Data(v.as_bytes().to_vec()));
+        Ok(())
+    }
+
+    fn serialize_bytes(self, v: &[u8]) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_none(self) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_some<T>(self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        todo!()
+    }
+
+    fn serialize_unit(self) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_unit_struct(self, _name: &'static str) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_unit_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+    ) -> Result<()> {
+        todo!()
+    }
+
+    fn serialize_newtype_struct<T>(self, _name: &'static str, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        todo!()
+    }
+
+    fn serialize_newtype_variant<T>(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+        value: &T,
+    ) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        todo!()
+    }
+
+    fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq> {
+        Ok(self)
+    }
+
+    fn serialize_tuple(self, _len: usize) -> Result<Self::SerializeTuple> {
+        Ok(self)
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        _name: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleStruct> {
+        Ok(self)
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleVariant> {
+        Ok(self)
+    }
+
+    fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap> {
+        Ok(self)
+    }
+
+    fn serialize_struct(self, _name: &'static str, len: usize) -> Result<Self::SerializeStruct> {
+        Ok(self)
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeStructVariant> {
+        Ok(self)
+    }
+}
+
+impl<'a> ser::SerializeSeq for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        todo!()
+    }
+
+    fn end(self) -> Result<()> {
+        todo!()
+    }
+}
+
+impl<'a> ser::SerializeTuple for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        todo!()
+    }
+
+    fn end(self) -> Result<()> {
+        todo!()
+    }
+}
+
+impl<'a> ser::SerializeTupleStruct for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        todo!()
+    }
+
+    fn end(self) -> Result<()> {
+        todo!()
+    }
+}
+
+impl<'a> ser::SerializeTupleVariant for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        todo!()
+    }
+
+    fn end(self) -> Result<()> {
+        todo!()
+    }
+}
+
+impl<'a> ser::SerializeMap for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_key<T>(&mut self, key: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        todo!()
+    }
+
+    fn serialize_value<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        todo!()
+    }
+
+    fn end(self) -> Result<()> {
+        todo!()
+    }
+}
+
+// Structs are like maps in which the keys are constrained to be compile-time
+// constant strings.
+impl<'a> ser::SerializeStruct for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, key: &'static str, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        key.serialize(&mut **self)?;
+        value.serialize(&mut **self)?;
+        Ok(())
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+// Similar to `SerializeTupleVariant`, here the `end` method is responsible for
+// closing both of the curly braces opened by `serialize_struct_variant`.
+impl<'a> ser::SerializeStructVariant for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, key: &'static str, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        key.serialize(&mut **self)?;
+        value.serialize(&mut **self)?;
+        Ok(())
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
---
+++ tests/serde.rs
@@ -0,0 +1,28 @@
+#[macro_use]
+extern crate serde_derive;
+
+use redis::Value;
+use serde::{Deserialize, Serialize};
+use serde_redis::{Deserializer, Serializer};
+
+#[test]
+fn test_serde_struct() {
+    #[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+    struct Foo {
+        f1: String,
+        // f2: i32,
+        // f3: i64,
+    }
+
+    let original = Foo {
+        f1: "hello world".to_owned(),
+    };
+    let v = serde_redis::encode::to_vec(&original).unwrap();
+    let data = &Value::Bulk(v);
+    let de = Deserializer::new(data);
+    let decoded: Foo = Deserialize::deserialize(de).unwrap();
+
+    println!("{:?}", original);
+    println!("{:?}", decoded);
+    assert_eq!(original, decoded);
+}
